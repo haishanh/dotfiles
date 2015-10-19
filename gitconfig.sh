3c850e081ba4e368e2010854626bdbae90295be0
haishanh <haishanhan@gmail.com>
2015-10-19 18:46:26 +0800

    Add gitconfig.sh

---
+++ gitconfig.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# ~/.gitconfig
+
+git config --global user.name "haishanh"
+git config --global user.email haishanhan@gmail.com
+
+git config --global core.editor vim
+
+git config --global color.ui true
+
+# alias
+git config --global alias.st status
+git config --global alias.ci commit 
