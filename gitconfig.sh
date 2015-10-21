1dc7627422647f91a81e6b967dc970cdf8096156
haishanh <haishanhan@gmail.com>
2015-10-22 01:02:21 +1100

    Add local option for gitconfig

--- gitconfig.sh
+++ gitconfig.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
 # ~/.gitconfig
 
-git config --global user.name "haishanh"
-git config --global user.email haishanhan@gmail.com
 
-git config --global core.editor vim
+git_config()
+{
+  global=${1}
+  git config${global} user.name "haishanh"
+  git config${global} user.email haishanhan@gmail.com
 
-git config --global color.ui true
+  git config${global} core.editor vim
 
-# alias
-git config --global alias.st status
-git config --global alias.ci commit 
+  git config${global} color.ui true
+
+  # alias
+  git config${global} alias.st status
+  git config${global} alias.ci commit 
+  git config${global} alias.d diff
+}
+
+if [ -z ${1} ]; then
+  echo "parameter needed"
+  exit 1
+fi
+
+if [ ${1} == "local" ]; then
+  git_config ""
+  exit 0
+fi
+
+if [ ${1} == "global" ]; then
+  git_config " --global"
+  exit 0
+fi
+
