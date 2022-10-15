5c99e374eac635800c4dae5408c61454f69352ac
Haishan <haishanhan@gmail.com>
2022-10-15 22:19:02 +0800

    config

--- nvim/lua/plugins/config.lua
+++ nvim/lua/plugins/config.lua
@@ -176,7 +176,8 @@ function M.config_lsp()
     )
   end
 
-  local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  -- local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
   -- local opts = {
   --   flags = { debounce_text_changes = 200 },
   --   capabilities = capabilities
