8cc671a62813cd083de868806f71d03c56b0948f
haishan <haishanhan@gmail.com>
2023-09-30 23:25:54 +0800

    indent-blankline

--- nvim/lua/lazy_plugins/indent-blankline.lua
+++ nvim/lua/lazy_plugins/indent-blankline.lua
@@ -1,36 +1,47 @@
 local highlight = {
-  "RainbowRed",
-  "RainbowYellow",
-  "RainbowBlue",
-  "RainbowOrange",
-  "RainbowGreen",
-  "RainbowViolet",
-  "RainbowCyan",
+  "Ibl0",
+  "Ibl1",
+  "Ibl2",
+  "Ibl3",
+  "Ibl4",
+  "Ibl5",
+  "Ibl6",
 }
-
 
 return {
   "lukas-reineke/indent-blankline.nvim",
   event = { "BufReadPost", "BufNewFile" },
   config = function()
     local hooks = require "ibl.hooks"
+    local h0 = { fg = "#515151" }
+    local h1 = { fg = "#333333" }
+
     -- create the highlight groups in the highlight setup hook, so they are reset
     -- every time the colorscheme changes
     hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-      vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-      vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-      vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-      vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-      vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-      vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-      vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+      vim.api.nvim_set_hl(0, "Ibl0", h0)
+      vim.api.nvim_set_hl(0, "Ibl1", h1)
+      vim.api.nvim_set_hl(0, "Ibl2", h0)
+      vim.api.nvim_set_hl(0, "Ibl3", h1)
+      vim.api.nvim_set_hl(0, "Ibl4", h0)
+      vim.api.nvim_set_hl(0, "Ibl5", h1)
+      vim.api.nvim_set_hl(0, "Ibl6", h0)
     end)
     require('ibl').setup({
-      char = "│",
       filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
       show_trailing_blankline_indent = false,
       show_current_context = false,
-      indent = { highlight = highlight },
+      indent = {
+        highlight = highlight,
+        -- char = "┊",
+        char = "│",
+      },
+      scope = {
+        enabled = true,
+      },
+      -- exclude = {
+      --   filetypes: {''}
+      -- },
     })
   end,
 }
