051d72fc84957dcfa99ad88f0d22bad07215dcc9
haishan <haishanhan@gmail.com>
2025-05-01 00:39:12 +0800

    uv config fish_variables gitconfig

--- fish/config.fish
+++ fish/config.fish
@@ -44,3 +44,7 @@ zoxide init fish | source
 
 alias ls eza
 # alias vim nvim
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
