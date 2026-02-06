f5c7a80d593689a596edc4f776dbc43989be5763
haishan <haishanhan@gmail.com>
2026-02-06 18:01:53 +0800

    cs

--- fish/functions/cs.fish
+++ fish/functions/cs.fish
@@ -7,7 +7,17 @@ function cs --argument-names cmd
             echo "gcloud config configurations list"
             echo "gcloud config configurations activate xxx"
         case http
-            echo "# TODO"
+            echo "http PUT pie.dev/put"
+            echo "  x-date:today  # he "
+            echo "  token==secret # Query"
+            echo "  name=John     # Data field"
+            echo "  "'"'ids[]=10'"'   "   # ids[0]=10"
+            echo "  ids[]=10 # ids[1]=20"
+            echo "  age:=29 # Raw JSON"
+            echo ""
+            echo "# application/x-www-form-urlencoded"
+            echo "# --data-urlencode"
+            echo "http --form POST httpbin.org/anything name=foo"
         case "" -h --help
             echo "Usage: cs command  Show help for command"
             echo "       cs gcloud"
